329268a0871e6b3fe149f3f6f3c12f6fab15e862
Astrid <astrid.beyer@etu.univ-amu.fr>
2026-03-18 23:52:50 +0100

    ADD: route mentions légales

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navbar';
-import { Footer } from './components/Footer';
-import { Home } from './pages/Home';
-import { Shows } from './pages/Shows';
-import { Blog } from './pages/Blog';
-import { Downloads } from './pages/Downloads';
-import { About } from './pages/About';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
+import { Shows } from "./pages/Shows";
+import { Blog } from "./pages/Blog";
+import { Downloads } from "./pages/Downloads";
+import { About } from "./pages/About";
+import { Legal } from "./pages/Legal";
 
 export default function App() {
   return (
     <Router basename={import.meta.env.BASE_URL}>
       <div className="min-h-screen flex flex-col relative">
         <div className="scanline"></div>
-        
+
         <Navbar />
-        
+
         <main className="flex-1 container mx-auto px-4 py-8 relative z-20">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -23,6 +24,7 @@ export default function App() {
             <Route path="/blog/:id" element={<Blog />} />
             <Route path="/downloads" element={<Downloads />} />
             <Route path="/about" element={<About />} />
+            <Route path="/mentions-legales" element={<Legal />} />
           </Routes>
         </main>
 
