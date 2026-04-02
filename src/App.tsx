e7a2a82315278b4d1f3917c898ca475dedefe07b
Vincent <49844372+vincequene@users.noreply.github.com>
2026-04-02 09:03:48 +0200

    Retour sur BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
@@ -7,32 +8,55 @@ import { Blog } from "./pages/Blog";
 import { Downloads } from "./pages/Downloads";
 import { About } from "./pages/About";
 import { Legal } from "./pages/Legal";
+import { NotFound } from "./pages/NotFound";
 import { ScrollToTopButton } from "./components/ScrollToTopButton";
+import { HelmetProvider } from "react-helmet-async";
+
+function RedirectHandler() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const redirect = params.get("redirect");
+
+    if (redirect) {
+      navigate(redirect, { replace: true });
+    }
+  }, [location.search, navigate]);
+
+  return null;
+}
 
 export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col relative">
-        <div className="scanline" aria-hidden="true"></div>
+    <HelmetProvider>
+      <Router>
+        <RedirectHandler />
 
-        <Navbar />
+        <div className="min-h-screen flex flex-col relative">
+          <div className="scanline" aria-hidden="true"></div>
 
-        <main className="flex-1 container mx-auto px-4 py-8 relative z-20">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/shows" element={<Shows />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/blog/:id" element={<Blog />} />
-            <Route path="/downloads" element={<Downloads />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/mentions-legales" element={<Legal />} />
-          </Routes>
-        </main>
+          <Navbar />
 
-        <ScrollToTopButton />
+          <main className="flex-1 container mx-auto px-4 py-8 relative z-20">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/shows" element={<Shows />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/blog/:id" element={<Blog />} />
+              <Route path="/downloads" element={<Downloads />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/mentions-legales" element={<Legal />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
 
-        <Footer />
-      </div>
-    </Router>
+          <ScrollToTopButton />
+
+          <Footer />
+        </div>
+      </Router>
+    </HelmetProvider>
   );
 }
